0dbf964845eaf763f8302297e45d453c256b2cd8
Martin Rodriguez <neomartinr@gmail.com>
2022-11-14 20:49:30 -0300

    ddasdas

--- js/products-list.js
+++ js/products-list.js
@@ -2,16 +2,16 @@
 //*Traer tabla y formulario
 //*Crear funcion para pintar tabla
 
-const Products =  JSON.parse(localStorage.getItem('products')) || [];
+const Products = JSON.parse(localStorage.getItem('products')) || [];
 const tableBodyHTML = document.getElementById('table-body-zapas');
 const AddProductForm = document.querySelector('#FormProducts');
 let editable;
 
 
-function agregarProducts(){
-    tableBodyHTML.innerHTML ='';
+function agregarProducts() {
+    tableBodyHTML.innerHTML = '';
     Products.forEach((elem, index) => {
-        tableBodyHTML.innerHTML +=  `<tr>
+        tableBodyHTML.innerHTML += `<tr>
         <td>
             <img src="${elem.image}" alt="" class="table-img img-fluid responsive">
         </td>
@@ -41,35 +41,35 @@ function agregarProducts(){
 agregarProducts();
 
 
-AddProductForm.addEventListener('submit', (event) =>{
+AddProductForm.addEventListener('submit', (event) => {
 
 
-if(AddProductForm.checkValidity()=== false){
-    return;
-}
+    if (AddProductForm.checkValidity() === false) {
+        return;
+    }
 
 
-event.preventDefault();
-console.dir (event.target.elements);
+    event.preventDefault();
+    console.dir(event.target.elements);
 
-const forElements = event.target.elements;
-console.dir(event)
-let newProduct = {
-    name: forElements.name.value,
-    description: forElements.description.value,
-    price: forElements.price.valueAsNumber,
-    image: forElements.image.value,
-    stock: forElements.stock.checked,
-    favorito: forElements.favorito.checked,
-    categoria:forElements.categoria.value,
-}
+    const forElements = event.target.elements;
+    console.dir(event)
+    let newProduct = {
+        name: forElements.name.value,
+        description: forElements.description.value,
+        price: forElements.price.valueAsNumber,
+        image: forElements.image.value,
+        stock: forElements.stock.checked,
+        favorito: forElements.favorito.checked,
+        categoria: forElements.categoria.value,
+    }
 
 
-console.log('Nuevo producto cargado')
-agregarProducts();
-localStorage.setItem('products',JSON.stringify(Products));
+    console.log('Nuevo producto cargado')
+    agregarProducts();
+    localStorage.setItem('products', JSON.stringify(Products));
 
-const formData = new FormData(AddProductForm);
+    const formData = new FormData(AddProductForm);
     const otroProduct = Object.fromEntries(formData)
 
     newProduct.price = parseInt(otroProduct.price)
@@ -79,7 +79,7 @@ const formData = new FormData(AddProductForm);
     newProduct.favorito = !!otroProduct.favorito;
     newProduct.stock = !!otroProduct.stock;
 
-    if(editable) {
+    if (editable >= 0) {
         Products[editable] = newProduct;
     } else {
         Products.push(newProduct)
@@ -92,7 +92,7 @@ const formData = new FormData(AddProductForm);
     localStorage.setItem('products', JSON.stringify(Products))
     agregarProducts();
     AddProductForm.reset()
-    
+
 })
 
 
@@ -112,42 +112,42 @@ function editProduct(idx) {
     formEl.name.value = productoAEditar.name;
     formEl.description.value = productoAEditar.description;
     formEl.price.value = productoAEditar.price;
-    formEl.image.value =  productoAEditar.image;
+    formEl.image.value = productoAEditar.image;
     formEl.stock.checked = productoAEditar.stock;
     formEl.favorito.checked = productoAEditar.favorito;
     formEl.categoria.value = productoAEditar.categoria;
 
-    
-    console.log(productoAEditar)
-    
-        AddProductForm.setAttribute('edit', idx); 
-        const prod = Products[idx]
 
-        Object.keys(prod).forEach(key => {
-            if( 'checked' in AddProductForm.elements[key]) {
+    console.log(productoAEditar)
+
+    AddProductForm.setAttribute('edit', idx);
+    const prod = Products[idx]
+
+    Object.keys(prod).forEach(key => {
+        if ('checked' in AddProductForm.elements[key]) {
             console.log(key, !!prod[key])
             AddProductForm.elements[key].checked = prod[key]
-            
-            }
-            AddProductForm.elements[key].value = Products[idx][key]
-            
-        })
-        AddProductForm.setAttribute('data-edit', idx);
-        deleteProduct(idx)
-    localStorage.setItem('products',JSON.stringify(Products));
+
+        }
+        AddProductForm.elements[key].value = Products[idx][key]
+
+    })
+    AddProductForm.setAttribute('data-edit', idx);
+    editable = idx;
+    // deleteProduct(idx)
+    // localStorage.setItem('products', JSON.stringify(Products));
 
 }
 
-    function setFavorite(idx) {
-        console.log('Producto favorito')
-        Products.forEach((prod) => {
-            prod.favorito= false;
-        })
-        Products[idx].favorito = true;
+function setFavorite(idx) {
+    console.log('Producto favorito')
+    Products.forEach((prod) => {
+        prod.favorito = false;
+    })
+    Products[idx].favorito = true;
 
 
-        console.log(Products);
-        agregarProducts()
-        localStorage.setItem('products', JSON.stringify(Products))
-    }
-
+    console.log(Products);
+    agregarProducts()
+    localStorage.setItem('products', JSON.stringify(Products))
+}
